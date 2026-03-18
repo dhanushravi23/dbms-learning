b649525adbe900a500d59ea98461d319d942eb2c
Dhanush R <dhanushravi2312@gmail.com>
2026-03-18 21:52:14 +0530

    Update insert-data.sql

--- 01-basic-sql/insert-data.sql
+++ 01-basic-sql/insert-data.sql
@@ -1,1 +1,14 @@
+insert into department (dept_name)
+values ('ise'), ('cse');
 
+insert into student (name, age, department_id)
+values ('dhanush', 20, 1),
+       ('rahul', 21, 2);
+
+insert into course (course_name, credits)
+values ('java', 3), ('dbms', 4);
+
+insert into enrollment (student_id, course_id)
+values (1, 1),
+       (1, 2),
+       (2, 1);
