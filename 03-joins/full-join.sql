0ca37291bdddbfdbe5fa2170a5f6bee886dd395f
Dhanush R <dhanushravi2312@gmail.com>
2026-03-18 23:03:59 +0530

    Update full-join.sql

--- 03-joins/full-join.sql
+++ 03-joins/full-join.sql
@@ -1,1 +1,11 @@
+select s.name, d.dept_name
+from student s
+left join department d
+on s.department_id = d.department_id
 
+union
+
+select s.name, d.dept_name
+from student s
+right join department d
+on s.department_id = d.department_id;
