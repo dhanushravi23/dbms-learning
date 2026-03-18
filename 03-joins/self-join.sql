2b946754452f3cd8c92b4c07e390bad43cd38c96
Dhanush R <dhanushravi2312@gmail.com>
2026-03-18 23:05:31 +0530

    Add mentor_id to student and create self-join query

--- 03-joins/self-join.sql
+++ 03-joins/self-join.sql
@@ -1,1 +1,9 @@
+alter table student
+add mentor_id int;
 
+update student set mentor_id = 1 where student_id = 2;
+
+select s1.name as student, s2.name as mentor
+from student s1
+join student s2
+on s1.mentor_id = s2.student_id;
